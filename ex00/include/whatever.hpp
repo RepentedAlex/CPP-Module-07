148fe85936c7db648f1dcf7b49e30e6ad356501a
apetitco <apetitco@student.42.fr>
2025-09-02 01:17:05 +0200

    Working on ex00

---
+++ ex00/include/whatever.hpp
@@ -0,0 +1,24 @@
+/// @file whatever.hpp
+/// @brief
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void	swap(T& a, T& b) {
+	T	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T	min(const T& a, const T& b) {
+	return (a < b) ? a : b;
+}
+
+template <typename T>
+T	max(const T& a, const T& b) {
+	return (a > b) ? a : b;
+}
+
+#endif // !WHATEVER_HPP
